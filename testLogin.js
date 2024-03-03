7f75ead4c52dae1a49ea2b63dc1cdbfbf50263d1
Ajayrahulapiswi <43535887+Ajayrahulapiswi@users.noreply.github.com>
2024-03-03 22:04:35 +0530

    Create testLogin.js

---
+++ testLogin.js
@@ -0,0 +1,28 @@
+const { openBrowser, goto, write, click, closeBrowser } = require('taiko');
+const assert = require('assert')
+
+async function testLogin() {
+  try {
+    // Open the browser and navigate to your website
+    await openBrowser();
+    await goto('https://practicetestautomation.com/practice-test-login/');
+
+    // Enter username and password, then click the login button
+    await click('Username')
+    await write('student');
+    await click('Password')
+    await write('Password123');
+    await click('submit');
+
+    // Add assertions or further testing as needed
+    // Example: assert the user is redirected to the dashboard
+    const currentUrl = await currentURL();
+    assert.strictEqual(currentUrl, 'https://practicetestautomation.com/logged-in-successfully/','Login failed');
+  } finally {
+    // Close the browser after the test
+    await closeBrowser();
+  }
+}
+
+// Run the test
+testLogin();
