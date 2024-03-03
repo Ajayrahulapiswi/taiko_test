35c54de7bbcdce95c2acdd9fdd1a6155ea9e03d1
Ajayrahulapiswi <43535887+Ajayrahulapiswi@users.noreply.github.com>
2024-03-03 22:13:51 +0530

    Update testLogin.js with report

--- testLogin.js
+++ testLogin.js
@@ -2,26 +2,33 @@ const { openBrowser, goto, write, click, closeBrowser } = require('taiko');
 const assert = require('assert')
 
 async function testLogin() {
-  try {
-    // Open the browser and navigate to your website
-    await openBrowser();
-    await goto('https://practicetestautomation.com/practice-test-login/');
+    let testPassed = true;
+    try {
+        // Open the browser and navigate to your website
+        await openBrowser();
+        await goto('https://practicetestautomation.com/practice-test-login/');
 
-    // Enter username and password, then click the login button
-    await click('Username')
-    await write('student');
-    await click('Password')
-    await write('Password1234');
-    await click('submit');
+        // Enter username and password, then click the login button
+        await click('Username')
+        await write('student');
+        await click('Password')
+        await write('Password123');
+        await click('submit');
 
-    // Add assertions or further testing as needed
-    // Example: assert the user is redirected to the dashboard
-    const currentUrl = await currentURL();
-    assert.strictEqual(currentUrl, 'https://practicetestautomation.com/logged-in-successfully/','Login failed');
-  } finally {
+        // Add assertions or further testing as needed
+        // Example: assert the user is redirected to the dashboard
+        const currentUrl = await currentURL();
+        assert.strictEqual(currentUrl, 'https://practicetestautomation.com/logged-in-successfully/','Login failed');
+    }
+    catch(error){
+        console.error(error.message)
+        testPassed=false
+    }
+    finally {
     // Close the browser after the test
     await closeBrowser();
   }
+  process.exit(testPassed?0:1);
 }
 
 // Run the test
